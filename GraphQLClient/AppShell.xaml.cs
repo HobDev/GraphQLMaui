646fc20ce1e966be4a01ed0a4837e5c83eb656e9
Paramjit <sparamjit92@gmail.com>
2025-05-11 15:28:58 +0530

    Initial commit

---
+++ GraphQLClient/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace GraphQLClient;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ GraphQLMaui/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace GraphQLMaui;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
